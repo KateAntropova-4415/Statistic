b1658a03b93ee71ccb167ccf685670900caaf492
Ekaterina Antropova <56113701+KateAntropova-4415@users.noreply.github.com>
2023-12-01 18:46:15 +0300

    more function

--- hypothesis_test.py
+++ hypothesis_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.stats import stats
+from statsmodels.stats import power as pwr
 
 
 def plot(data1, data2):
@@ -15,6 +16,7 @@ def plot(data1, data2):
     plt.show()
 
 
+
 def t_test(data1, data2, alpha):
     t_stat, p_value = stats.ttest_ind(data1, data2)
     print(f"Значение t-статистики: {t_stat}")
@@ -24,7 +26,35 @@ def t_test(data1, data2, alpha):
     else:
         print("Разница между средними не значима")
 
+def mean_12(data1, data2):
+    print("Среднее для data1:", data1.mean())
+    print("Среднее для data2: ", data2.mean())
+
+
+def size(data1, data2, alpha, power):
+    # Calculate conversion rate mean and std
+    data1_mean = data1.mean()
+    data1_std = data1.std()
+
+    # Setting the parameters and we want to increase the purchase_mean to 0.1 in this experiment
+    effect_size = (0.1 - data1_mean) / data1_std
+
+    # Calculate ratio
+    test_n = len(data2)
+    cont_n = len(data1)
+    sizes = [cont_n, test_n]
+    ratio = max(sizes) / min(sizes)
+
+    # Initialize analysis and calculate sample size
+    analysis = pwr.TTestIndPower()
+    ssresult = analysis.solve_power(effect_size=effect_size, power=power, alpha=alpha, nobs1=None, ratio=ratio)
+
+    print(f'Sample Size: {int(ssresult)}')
+
+
 # проверка статистической значимости I
 # Теперь, когда у нас есть интуитивное представление о статистической значимости и p-значениях, мы применим его к данным о результатах нашего тестирования.
 # Здесь мы рассчитываем размер тестовой и контрольной групп и их соответствующие коэффициенты конверсии.
-def stat_sign_I(data1, data2):
+
+#def stat_sign_I(data1, data2):
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import hypothesis_test
+import hypothesis_test as ab_test
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
@@ -6,8 +6,12 @@ warnings.filterwarnings('ignore')
 np.random.seed(42)
 data1 = np.random.normal(loc=5, scale=2, size=100)
 data2 = np.random.normal(loc=6, scale=2, size=100)
+print("data1: ", data1)
+print("data2: ", data2)
 alpha = 0.05
 
-hypothesis_test.plot(data1, data2)
-hypothesis_test.t_test(data1, data2, alpha)
+ab_test.mean_12(data1, data2)
+ab_test.plot(data1, data2)
+ab_test.t_test(data1, data2, alpha)
+ab_test.size(data1, data2, alpha, 0.8)
 
